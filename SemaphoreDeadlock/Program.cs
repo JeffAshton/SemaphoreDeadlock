3ae228b61fa6df209162500abe1e51ac5ff5bb27
Jeff Ashton <JeffAshton@users.noreply.github.com>
2021-03-26 11:12:56 -0400

    Example of how SemaphoreSlim deadlocks

---
+++ SemaphoreDeadlock/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace SemaphoreDeadlock {
+	class Program {
+		static void Main() {
+
+			using( ManualResetEvent hasSemaphoreEvent = new ManualResetEvent( false ) ) {
+
+				using( SemaphoreSlim semaphore = new SemaphoreSlim( 1 ) ) {
+					Console.WriteLine( $"Created semaphore [ CurrentCount: { semaphore.CurrentCount } ]" );
+
+					Thread thread = new Thread( () => {
+						try {
+							Console.WriteLine( $"Waiting on semaphore [ CurrentCount: { semaphore.CurrentCount } ]" );
+							semaphore.Wait();
+
+							Console.WriteLine( $"Semaphore acquired [ CurrentCount: { semaphore.CurrentCount } ] " );
+							hasSemaphoreEvent.Set();
+
+							// Simulate case where try isn't entered in time
+							Thread.Sleep( TimeSpan.FromSeconds( 5 ) );
+
+							try {
+								Console.WriteLine( $"Inside try [ CurrentCount: { semaphore.CurrentCount } ] " );
+
+							} finally {
+								Console.WriteLine( $"Releasing semaphore [ CurrentCount: { semaphore.CurrentCount } ]" );
+								semaphore.Release();
+							}
+
+						} catch ( ThreadAbortException ) {
+							Console.WriteLine( $"Thread aborted  [ CurrentCount: { semaphore.CurrentCount } ] " );
+						}
+					} );
+
+					thread.Start();
+					hasSemaphoreEvent.WaitOne();
+					thread.Abort();
+					thread.Join();
+
+					Console.WriteLine( $"Thread completed  [ CurrentCount: { semaphore.CurrentCount } ] " );
+					if( semaphore.CurrentCount == 0 ) {
+						Console.WriteLine( "Semaphore deadlocked. Checkmate!!!" );
+					}
+				}
+			}
+		}
+	}
+}
